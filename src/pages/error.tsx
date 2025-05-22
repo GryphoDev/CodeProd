073bb4fd06ff3a017762173d709545f378f5f05f
GryphoDev <wmauroux@icloud.com>
2025-05-22 10:59:35 +0200

    fix: error image

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,7 +1,7 @@
 export function Error() {
   return (
     <div className="flex justify-center items-center">
-      <img className="w-1/3 ml-10" src="public/error404.png" alt="" />
+      <img className="w-1/3 ml-10" src="/error404.png" alt="" />
     </div>
   );
 }
